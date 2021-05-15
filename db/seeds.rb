f6660ac604c31683c9674ffbc70d185247f6b098
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-05-15 19:34:07 -0400

    seed database; create destroy all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+10.times do
+    User.create(name: Faker::TvShows::StarTrek.character, username: Faker::TvShows::AquaTeenHungerForce.character, email: Faker::Internet.safe_email, celiac: nil, adventurous: nil)
+end
+
+puts "
+
+Noice! 
+"
