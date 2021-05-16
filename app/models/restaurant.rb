409d6c3dbd59e95a21333ff24b80a85d3c480b7f
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-05-16 18:31:47 -0400

    defined has_many/belongs_to relationships

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+    belongs_to_many :trips
+    belongs_to :destination
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ApplicationRecord
+    belongs_to_many :users
+    belongs_to :destinations
+    has_many :restaurants
 end
