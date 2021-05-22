a354667ea9486f90c8f9830142559204e20bb7c8
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-05-22 12:52:35 -0400

    added sessions controller; set up route for omniauth to return

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+    #logging a user in/out, omniauth
+    def OmniAuth #log users in with omniauth
+        binding.pry
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :destinations
   resources :trips
   resources :users
+
+  get '/auth/:provider/callback' to: 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
