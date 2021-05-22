c6824d2f9b4f1a26bf8a7703939bced96940a0e3
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-05-22 12:17:45 -0400

    removed belongs_to_many from Restaurants & Trips; added password digest, uid, provider columns to Users table for OmniAuth:

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
-    belongs_to_many :trips
+    belongs_to :trips
     belongs_to :destination
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-    belongs_to_many :users
+    belongs_to :users
     belongs_to :destination
     has_many :restaurants
 end
