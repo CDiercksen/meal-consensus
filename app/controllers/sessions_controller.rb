2fa4f426d1ba688aa0a716a34f8e2d8394a87462
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-05-22 20:09:51 -0400

    debugged def omniauth in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
     #logging a user in/out, omniauth
-    def omniauth #log users in with omniauth
-        binding.pry
+    def omniauth
+    user = User.create_from_omniauth(auth)
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to 'show'
+        else
+            flash[:message] = user.errors.full_messages.join(", ")
+            redirect_to 'users'
+        end
+    end
+
+    private
+    def auth
+        request.env['omniauth.auth']
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :trips
     has_many :restaurants, through: :trips
+    
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+    end
 end
