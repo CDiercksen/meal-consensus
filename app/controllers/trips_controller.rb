3c1bd38aff4145c4668fc6ae27389dffa207ff89
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-06-03 16:12:29 -0400

    nested resource 'trips' within 'users'

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,22 +4,28 @@ class TripsController < ApplicationController
     end
 
     def new #gets the form
-        @trip = Trip.new
-        @trip.build_category
-        3.times do
-            d = @trip.destination.build
-            d.build_destination
-        end
+        @user = current_user
+        @trip = @user.trips.build
+        # 3.times do
+        #     d = @trip.destination.build
+        #     d.build_destination
+        # end
     end
 
     def create #posts the form
-        @trip = Trip.new(trip_params)
+        @user = current_user
+        @trip = @user.trips.build(trip_params)
         if @trip.save
-            redirect_to trip_path(@trip)
+           
+            # redirect_to trip_path(@trip)
+            redirect_to user_trip_path(@user, @trip)
+        else
+            binding.pry
+            render 'new'
         end
     end
 
     def trip_params
-        params.require(:trip).permit(:name, :user_ids:[], :destination_id)
+        params.require(:trip).permit(:name, :user_id, :destination_id)
     end
 end
