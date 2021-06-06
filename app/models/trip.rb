cb3c5d0201c3d8f4e5fbaa8828529554ecadcfbb
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-06-05 21:03:32 -0400

    made destination an optional relationship for Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
     belongs_to :user
-    belongs_to :destination
+    belongs_to :destination, optional: true
     #has_many :restaurants, through: :destination
     validates :name, uniqueness: true, presence: true
 end
