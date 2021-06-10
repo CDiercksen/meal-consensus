70ddd07e13be363b6730ad1b723039f9270f1089
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-06-10 18:22:35 -0400

    debugged new trip form by removing destination creation from responsibilities

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,15 @@
 class TripsController < ApplicationController
     def index
         @trips = Trip.all
+        # binding.pry
     end
 
     def new #gets the form
         @user = current_user
         @trip = @user.trips.build
         #@destination = Destination.new
-        Destination.new 
+        @destination = Destination.new 
+        
         # 3.times do
         #     d = @trip.destination.build
         #     d.build_destination
@@ -17,10 +19,12 @@ class TripsController < ApplicationController
     def create #posts the form
         @user = current_user
         @trip = @user.trips.build(trip_params)
-       # binding.pry
+    #    binding.pry
         if @trip.save
-           
-            # redirect_to trip_path(@trip)
+            # @destination = Destination.new(destination_params)
+            # @destination.save
+            # @trip.destination_id = @destination.id
+            # binding.pry
             redirect_to user_trip_path(@user, @trip)
         else
            # binding.pry
@@ -30,6 +34,13 @@ class TripsController < ApplicationController
 
     def show
         @trip = Trip.find_by_id(params[:id])
+    end
+
+
+    private
+
+    def destination_params
+        params.require(:destination).permit(:name)
     end
 
     def trip_params
